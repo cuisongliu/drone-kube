806747945a0d5e81e5160f30225e0ce4f11dfb00
cuisongliu <cuisongliu@qq.com>
2019-03-19 17:16:42 +0800

    fix drone plugins

--- template/main.go
+++ template/main.go
@@ -1,9 +1,36 @@
 package config
 
+import (
+	"github.com/wonderivan/logger"
+	"io/ioutil"
+	"os"
+)
+
 // KubeDeployDir is var
 var KubeDeployDir string
 
 //Main is config command
 func Main() {
+	if !pathExists(KubeDeployDir) {
+		logger.Error("the dir is ", KubeDeployDir, ",not exits.")
+		return
+	}
+	files, _ := ioutil.ReadDir(KubeDeployDir)
+	if len(files) == 0 {
+		logger.Error("the dir is ", KubeDeployDir, ",empty.")
+		return
+	}
 
+}
+
+//pathExists is tools for file
+func pathExists(path string) bool {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true
+	}
+	if os.IsNotExist(err) {
+		return false
+	}
+	return false
 }
